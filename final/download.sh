c4d1a80ece83a5618cde6611355eafb1930db44c
PyYe <autumn11017@gmail.com>
2019-01-12 14:12:24 +0800

    Update

---
+++ final/download.sh
@@ -0,0 +1,4 @@
+wget -O ./src/ensemble/test_matches.csv https://www.dropbox.com/s/8jhjcl7lspzgq07/test_matches.csv?dl=1
+wget -O ./src/model/test1_notgenerator.h5 https://www.dropbox.com/s/wapr9tsj0lyncyd/test1_notgenerator.h5?dl=1
+wget -O ./src/model/test3_notgenerator_densenet201_dense.h5 https://www.dropbox.com/s/c7s0anepqnioom5/test3_notgenerator_densenet201_dense.h5?dl=1
+wget -O ./src/model/test5_notgenerator_DenseNet169_dense.h5 https://www.dropbox.com/s/almv0z3xdekiyvq/test5_notgenerator_DenseNet169_dense.h5?dl=1
